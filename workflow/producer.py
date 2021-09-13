a3e7dd5cbf6c700769f97317acc92071de369d60
Serghei Iakovlev <egrep@protonmail.ch>
2021-09-14 01:55:33 +0300

    Running a task after all tasks have been completed

--- workflow/producer.py
+++ workflow/producer.py
@@ -1,10 +1,11 @@
 from celery.result import AsyncResult
 from celery.canvas import Signature
+from celery import chord, group
 
-from .tasks import power
+from .tasks import amass, power
 
 
-def test_simple():
+def test_power():
     assert power(3, 4) == 81
 
     result = power.delay(3, 4)
@@ -16,6 +17,29 @@ def test_simple():
     print(result.__repr__())
 
 
+
+def test_complex():
+    tasks = []
+
+    for i in range(10):
+        tasks.append(power.s(i, 2))
+
+    # The ``group``` primitive is a signature that takes
+    # a list of tasks that should be applied in parallel.
+    tasks = group(tasks)
+
+    callback = amass.s()
+
+    # A ``chord``` is just like a ``group``` but with a callback.
+    # A ``chord``` consists of a header ``group``` and a body,
+    # where the body is a task that should execute after all of the
+    # tasks in the header are complete.
+    result = chord(tasks)(callback)
+    assert isinstance(result, AsyncResult)
+    print(result.__repr__())
+
+
 if __name__ == '__main__':
     print('Start testing workflow...')
-    test_simple()
+    # test_power()
+    test_complex()
--- workflow/tasks.py
+++ workflow/tasks.py
@@ -6,5 +6,10 @@ from .celery import app
 
 @app.task
 def power(value, expo):
-    sleep(random.randint(10, 1000) / 1000.0) # sleep for 10-1000ms
+    sleep(random.randint(10, 1000) / 100.0)
     return value ** expo
+
+
+@app.task
+def amass(values):
+    print(str(values))
