3ee736834e97452efb9acb3e7da35747eb096111
Serghei Iakovlev <egrep@protonmail.ch>
2021-09-14 10:54:39 +0300

    Print payload len

--- workflow/tasks.py
+++ workflow/tasks.py
@@ -6,10 +6,15 @@ from .celery import app
 
 @app.task
 def power(value, expo):
+    # TODO: Simulate tasks error
+    # if value == 7:
+    #    raise ValueError
+
     sleep(random.randint(10, 1000) / 100.0)
     return value ** expo
 
 
 @app.task
 def amass(values):
-    print(str(values))
+    print(len(values))
+    print(values)
