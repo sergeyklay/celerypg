80fd6b32eabdb7fbe6831b52091c76bf5d9acee3
Serghei Iakovlev <egrep@protonmail.ch>
2021-09-15 23:57:59 +0300

    Code cleanup

--- workflow/producer.py
+++ workflow/producer.py
@@ -1,23 +1,25 @@
-from celery import chord, group
+from celery import chord
 
 from .tasks import amass, power
 
 
 def main():
     # Create 10 tasks signatures.
-    # The ``group`` primitive is a signature that takes
-    # a list of tasks that should be applied in parallel.
-    tasks = group([power.s(i, 2) for i in range(10)])
+    tasks = (power.s(i, 2) for i in range(10))
 
     # Create a callback.
     callback = amass.s()
 
     # A ``chord`` is just like a ``group`` but with a callback.
-    # A ``chord`` consists of a header ``group`` and a body,
+    #
+    # A ``chord`` consists of a header group and a body,
     # where the body is a task that should execute after all of the
     # tasks in the header are complete.
-    r = chord(tasks)(callback)
-    print(r)
+    #
+    # Actually, the code bellow is equivalent the following
+    # >>> group = chord(tasks)
+    # >>> group(callback)
+    return chord(tasks)(callback)
 
 
 if __name__ == '__main__':
